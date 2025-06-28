b205f86bd723d38cfbdf797601f50966cd85a393
Tomas Merendon <39959095+TomyMeren@users.noreply.github.com>
2025-06-28 11:58:14 +0200

    add log4j2.propiertes file and GrpahFrame folder

---
+++ GraphFrame/GraphFrameQuickStart.scala
@@ -0,0 +1,44 @@
+import org.apache.spark.sql.SparkSession
+import org.graphframes._
+
+object GraphFrameQuickStart extends App {
+  val spark = SparkSession
+    .builder()
+    .appName("PlaySpark")
+    .master("local[*]")
+    .getOrCreate()
+
+// Create a Vertex DataFrame with unique ID column "id"
+  val v = spark
+    .createDataFrame(
+      List(
+        ("a", "Alice", 34),
+        ("b", "Bob", 36),
+        ("c", "Charlie", 30)
+      )
+    )
+    .toDF("id", "name", "age")
+
+// Create an Edge DataFrame with "src" and "dst" columns
+  val e = spark
+    .createDataFrame(
+      List(
+        ("a", "b", "friend"),
+        ("b", "c", "follow"),
+        ("c", "b", "follow")
+      )
+    )
+    .toDF("src", "dst", "relationship")
+// Create a GraphFrame
+  val g = org.graphframes.GraphFrame(v, e)
+
+// Query: Get in-degree of each vertex.
+  g.inDegrees // .show()
+
+// Query: Count the number of "follow" connections in the graph.
+  // println(g.edges.filter("relationship = 'follow'").count())
+
+// Run PageRank algorithm, and show results.
+  val results = g.pageRank.resetProbability(0.01).maxIter(20).run()
+  results.vertices.select("id", "pagerank").show()
+}
