74cde2d5d57261fa24455aa0598ef6e92dd71d15
Tomas Merendon <39959095+TomyMeren@users.noreply.github.com>
2025-06-30 18:30:34 +0200

    spark version

--- project.scala
+++ project.scala
@@ -1,5 +1,5 @@
-//> using dep org.apache.spark::spark-sql:3.5.6
-//> using dep org.apache.spark::spark-graphx:3.5.6
+//> using dep org.apache.spark::spark-sql:3.5.5
+//> using dep org.apache.spark::spark-graphx:3.5.5
 //> using scala 2.12.20
 //> using jvm 11
 //> using dep org.hablapps:doric_3-5_2.12:0.0.8
